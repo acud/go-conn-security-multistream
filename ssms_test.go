71b9f683a895ce0a7cc8ca3149a5a6f1ac62809e
vyzo <vyzo@hackzen.org>
2019-08-26 15:53:27 +0300

    fix test with transport adapter

--- ssms_test.go
+++ ssms_test.go
@@ -6,9 +6,25 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/sec"
 	"github.com/libp2p/go-libp2p-core/sec/insecure"
 	sst "github.com/libp2p/go-libp2p-testing/suites/sec"
 )
+
+type TransportAdapter struct {
+	mux *SSMuxer
+}
+
+func (sm *TransportAdapter) SecureInbound(ctx context.Context, insecure net.Conn) (sec.SecureConn, error) {
+	sconn, _, err := sm.mux.SecureInbound(ctx, insecure)
+	return sconn, err
+}
+
+func (sm *TransportAdapter) SecureOutbound(ctx context.Context, insecure net.Conn, p peer.ID) (sec.SecureConn, error) {
+	sconn, _, err := sm.mux.SecureOutbound(ctx, insecure, p)
+	return sconn, err
+}
 
 func TestCommonProto(t *testing.T) {
 	var at, bt SSMuxer
@@ -17,7 +33,7 @@ func TestCommonProto(t *testing.T) {
 	at.AddTransport("/plaintext/1.0.0", atInsecure)
 	bt.AddTransport("/plaintext/1.1.0", btInsecure)
 	bt.AddTransport("/plaintext/1.0.0", btInsecure)
-	sst.SubtestRW(t, &at, &bt, "peerA", "peerB")
+	sst.SubtestRW(t, &TransportAdapter{mux: &at}, &TransportAdapter{mux: &bt}, "peerA", "peerB")
 }
 
 func TestNoCommonProto(t *testing.T) {
@@ -37,7 +53,7 @@ func TestNoCommonProto(t *testing.T) {
 	go func() {
 		defer wg.Done()
 		defer a.Close()
-		_, err := at.SecureInbound(ctx, a)
+		_, _, err := at.SecureInbound(ctx, a)
 		if err == nil {
 			t.Fatal("conection should have failed")
 		}
@@ -46,7 +62,7 @@ func TestNoCommonProto(t *testing.T) {
 	go func() {
 		defer wg.Done()
 		defer b.Close()
-		_, err := bt.SecureOutbound(ctx, b, "peerA")
+		_, _, err := bt.SecureOutbound(ctx, b, "peerA")
 		if err == nil {
 			t.Fatal("connection should have failed")
 		}
